d0bda2f474195a72bd91c4e3df9ff502918dbbc1
Steven Masley <stevenmasley@gmail.com>
2024-04-05 18:15:46 -0500

    remove jest

--- src/test-utils/fakes/utils.ts
+++
@@ -1,5 +0,0 @@
-function dist(a: RoomPosition | RoomObject, b: RoomPosition | RoomObject) {
-  if ("pos" in a) a = a.pos;
-  if ("pos" in b) b = b.pos;
-  return Math.max(Math.abs(a.x - b.x), Math.abs(a.y - b.y));
-}
